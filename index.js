936e42d548550db97ef3243c4ca3df9e31127fd9
Bryce-Morris <bryce@vandelayeducation.com>
2017-07-11 14:19:37 -0500

    POST testing

--- index.js
+++ index.js
@@ -2,55 +2,31 @@ var http = require('http');
 var fs = require('fs');
 var path = require('path');
 
-http.createServer(function (request, response) {
-    console.log('request starting...');
+http.createServer( function(req, res) {
 
-    var filePath = '.' + request.url;
-    if (filePath === './'){
-        filePath = './page.html';}
+    console.dir(req.param);
 
-    var extname = path.extname(filePath);
-    var contentType = 'text/html';
-    switch (extname) {
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        case '.css':
-            contentType = 'text/css';
-            break;
-        case '.json':
-            contentType = 'application/json';
-            break;
-        case '.png':
-            contentType = 'image/png';
-            break;      
-        case '.jpg':
-            contentType = 'image/jpg';
-            break;
-        case '.wav':
-            contentType = 'audio/wav';
-            break;
-    } 
-
-    fs.readFile(filePath, function(error, content) {
-        if (error) {
-            if(error.code === 'ENOENT'){
-                fs.readFile('./404.html', function(error, content) {
-                    response.writeHead(200, { 'Content-Type': contentType });
-                    response.end(content, 'utf-8');
-                });
-            }
-            else {
-                response.writeHead(500);
-                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
-                response.end(); 
-            }
-        }
-        else {
-            response.writeHead(200, { 'Content-Type': contentType });
-            response.end(content, 'utf-8');
-        }
-    });
+    if (req.method == 'POST') {
+        console.log("POST");
+        var body = '';
+        req.on('data', function (data) {
+            body += data;
+            console.log("Partial body: " + body);
+        });
+        req.on('end', function () {
+            console.log("Body: " + body);
+        });
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('post received');
+    }
+    else
+    {
+        console.log("GET");
+        //var html = '<html><body><form method="post" action="http://localhost:3000">Name: <input type="text" name="name" /><input type="submit" value="Submit" /></form></body>';
+        var html = fs.readFileSync('index.html');
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(html);
+    }
 
 }).listen(process.env.PORT || 5000);
 console.log('Server running at port ' + process.env.PORT);
