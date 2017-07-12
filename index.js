380b2c88982abe1a502d8acb4ced36b3b0dcfe9b
Bryce-Morris <bryce@vandelayeducation.com>
2017-07-12 11:54:03 -0500

    .

--- index.js
+++ index.js
@@ -22,11 +22,46 @@ http.createServer( function(req, res) {
     }
     else
     {
-        console.log("GET");
-        //var html = '<html><body><form method="post" action="http://localhost:3000">Name: <input type="text" name="name" /><input type="submit" value="Submit" /></form></body>';
-        var html = fs.readFileSync('page.html');
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end(html);
+        var filePath = '.' + request.url;
+    if (filePath === './'){
+        filePath = './page.html';}
+
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;      
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+    }
+
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+            if(error.code === 'ENOENT'){
+                fs.readFile('./404.html', function(error, content) {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                });
+            }
+            else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+                response.end(); 
+            }
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
+    });
     }
 
 }).listen(process.env.PORT || 5000);
